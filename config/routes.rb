4c6ac1021e436b0ba9b6e07f8c6a0bb73ff8c0ef
bainur <you@example.com>
2021-01-04 15:51:54 +0700

    add page login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => 'registrations' }, :path => 'admin', :path_names => { :sign_in => 'login', :sign_up => 'new', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification' }
+  devise_for :users, :only => [:sessions], path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout'}
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+  
   #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
@@ -16,5 +20,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :movies
+    root 'movies#index'
   end
+
+  root "admin/movies#index"
 end
